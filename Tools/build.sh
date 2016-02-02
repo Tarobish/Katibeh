6e47c56765e2610210449e7460018832de67a8f5
Lasse Fister <commander@graphicore.de>
2016-02-03 00:59:13 +0100

    Port build process changes from Mirza

---
+++ Tools/build.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+echo building Katibeh
+./Tools/buildFont.sh \
+		Generated/Arabic/Katibeh-Regular.otf \
+		Generated/Latin/Katibeh-Latin-open-Demibold.otf \
+		Build/Katibeh-Regular.otf;

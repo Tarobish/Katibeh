5f7a94aba9fdd890321f0a12006b85c8d6f03921
Lasse Fister <commander@graphicore.de>
2016-02-25 23:58:23 +0100

    Build TTF from TTF sources.

--- Tools/build.sh
+++ Tools/build.sh
@@ -4,4 +4,7 @@ echo building Katibeh
 ./Tools/buildFont.sh \
 		Generated/Arabic/Katibeh-Regular.otf \
 		Generated/Latin/Katibeh-Latin-open-Demibold.otf \
-		Build/Katibeh-Regular.otf;
+		Build/Katibeh-Regular.otf \
+		Generated/Arabic/Katibeh-Regular.ttf \
+		Generated/Latin/Katibeh-Latin-open-Demibold.ttf \
+		Build/Katibeh-Regular.ttf;
